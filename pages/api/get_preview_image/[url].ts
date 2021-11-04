772970e187f52fc137ffb2536d60310f5242b148
Jorrin <jorrinkievit@hotmail.com>
2021-11-04 20:04:44 +0100

    Update [url].ts

--- pages/api/get_preview_image/[url].ts
+++ pages/api/get_preview_image/[url].ts
@@ -13,38 +13,38 @@ const handler = async (
 
   let base64Image = "";
 
-  try {
-    await new Pageres({ delay: 2 })
-      .src(url as string, [`${width}x${height}`], { crop: true })
-      .run()
-      .then((stream) => {
-        base64Image = stream[0].toString("base64");
-      });
-
-    const metaData = await parser(url as string);
-
-    const metaObject = {
-      title:
-        (metaData.meta?.title ? metaData.meta.title : metaData.og?.title) ??
-        "Not set by requested page",
-      description:
-        (metaData.meta?.description
-          ? metaData.meta.description
-          : metaData.og?.description) ?? "Not set by requested page",
-      url: (metaData.meta?.url ? metaData.meta?.url : metaData.og?.url) ?? url,
-      site_name: metaData.og?.site_name ?? "Not set by requested page",
-    };
-
-    res.status(200).json({
-      image: base64Image,
-      title: metaObject.title,
-      description: metaObject.description,
-      url: metaObject.url,
-      site_name: metaObject.site_name,
+  // try {
+  await new Pageres({ delay: 2 })
+    .src(url as string, [`${width}x${height}`], { crop: true })
+    .run()
+    .then((stream) => {
+      base64Image = stream[0].toString("base64");
     });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+
+  const metaData = await parser(url as string);
+
+  const metaObject = {
+    title:
+      (metaData.meta?.title ? metaData.meta.title : metaData.og?.title) ??
+      "Not set by requested page",
+    description:
+      (metaData.meta?.description
+        ? metaData.meta.description
+        : metaData.og?.description) ?? "Not set by requested page",
+    url: (metaData.meta?.url ? metaData.meta?.url : metaData.og?.url) ?? url,
+    site_name: metaData.og?.site_name ?? "Not set by requested page",
+  };
+
+  res.status(200).json({
+    image: base64Image,
+    title: metaObject.title,
+    description: metaObject.description,
+    url: metaObject.url,
+    site_name: metaObject.site_name,
+  });
+  // } catch (error) {
+  // res.status(500).json({ error });
+  // }
 };
 
 export default handler;
